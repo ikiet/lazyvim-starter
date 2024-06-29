cc5e41707482b1dba7384807887913eb8f6ca2ae
Kiet Cao <kietcao@tuta.io>
2024-06-29 22:08:12 +0900

    add telescope file browser

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,25 +1,19 @@
-local telescopeConfig = require("telescope.config")
-
--- Clone the default Telescope configuration
-local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
-
--- I want to search in hidden/dot files.
-table.insert(vimgrep_arguments, "--hidden")
--- I don't want to search in the `.git` directory.
-table.insert(vimgrep_arguments, "--glob")
-table.insert(vimgrep_arguments, "!**/.git/*")
-table.insert(vimgrep_arguments, "--glob")
-table.insert(vimgrep_arguments, "!**/.yarn/*")
-
 return {
   {
     "nvim-telescope/telescope.nvim",
+    dependencies = {
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+      },
+      "nvim-telescope/telescope-file-browser.nvim",
+    },
     keys = {
-      -- add a keymap to browse plugin files
-      -- stylua: ignore
       {
         "<leader>ff",
-        function() require("telescope.builtin").find_files() end,
+        function()
+          require("telescope.builtin").find_files()
+        end,
         desc = "Find File",
       },
       {
@@ -29,18 +23,40 @@ return {
         end,
         desc = "Find File (No Ignore)",
       },
-    },
-    -- change some options
-    opts = {
-      pickers = {
-        find_files = {
-          find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*", "--glob", "!**/.yarn/*" },
-        },
-        oldfiles = {
-          cwd_only = true,
-        },
+      {
+        "<leader>fp",
+        function()
+          require("telescope").extensions.file_browser.file_browser({
+            path = vim.fn.expand("%:p:h"),
+            select_buffer = true,
+            previewer = false,
+            respect_gitignore = false,
+            hidden = true,
+            grouped = true,
+            initial_mode = "normal",
+          })
+        end,
+        desc = "File Browser",
       },
-      defaults = {
+    },
+    config = function(_, opts)
+      local telescope = require("telescope")
+      local actions = require("telescope.actions")
+      local fb_actions = require("telescope").extensions.file_browser.actions
+
+      local configs = require("telescope.config")
+      -- Clone the default Telescope configuration
+      local vimgrep_arguments = { unpack(configs.values.vimgrep_arguments) }
+
+      -- I want to search in hidden/dot files.
+      table.insert(vimgrep_arguments, "--hidden")
+      -- I don't want to search in the `.git` directory.
+      table.insert(vimgrep_arguments, "--glob")
+      table.insert(vimgrep_arguments, "!**/.git/*")
+      table.insert(vimgrep_arguments, "--glob")
+      table.insert(vimgrep_arguments, "!**/.yarn/*")
+
+      opts.defaults = {
         mappings = {
           i = {
             ["<M-n>"] = require("telescope.actions").cycle_history_next,
@@ -57,7 +73,40 @@ return {
         layout_config = { prompt_position = "top" },
         sorting_strategy = "ascending",
         winblend = 0,
-      },
-    },
+      }
+      opts.pickers = {
+        find_files = {
+          find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*", "--glob", "!**/.yarn/*" },
+        },
+        oldfiles = {
+          cwd_only = true,
+        },
+      }
+      opts.extensions = {
+        file_browser = {
+          theme = "dropdown",
+          -- disables netrw and use telescope-file-browser in its place
+          hijack_netrw = false,
+          mappings = {
+            -- your custom insert mode mappings
+            ["n"] = {
+              -- your custom normal mode mappings
+              ["a"] = fb_actions.create,
+              ["h"] = fb_actions.goto_parent_dir,
+              ["c"] = fb_actions.copy,
+              ["d"] = fb_actions.remove,
+              ["r"] = fb_actions.rename,
+              ["m"] = fb_actions.move,
+              ["o"] = fb_actions.open,
+              ["/"] = fb_actions.goto_cwd,
+              ["~"] = fb_actions.goto_home_dir,
+            },
+          },
+        },
+      }
+      telescope.setup(opts)
+      require("telescope").load_extension("fzf")
+      require("telescope").load_extension("file_browser")
+    end,
   },
 }
