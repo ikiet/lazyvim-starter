3d14e8bf6b922a5fe618a75b124b21b09651a791
Kiet Cao <kietcao@tuta.io>
2024-06-29 20:45:00 +0900

    add copilot chat config file

---
+++ lua/plugins/copilot-chat.lua
@@ -0,0 +1,45 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    opts = {
+      window = {
+        width = 0.6,
+        height = 0.6,
+        layout = "float",
+        relative = "editor", -- 'editor', 'win', 'cursor', 'mouse'
+        border = "single", -- 'none', single', 'double', 'rounded', 'solid', 'shadow'
+      },
+      mappings = {
+        close = {
+          normal = "<Esc>",
+        },
+        reset = {
+          normal = "<C-l>",
+          insert = "<C-l>",
+        },
+        submit_prompt = {
+          normal = "<CR>",
+          insert = "<C-CR>",
+        },
+        accept_diff = {
+          normal = "<C-y>",
+          insert = "<C-y>",
+        },
+        yank_diff = {
+          normal = "gy",
+          insert = "<C-c>",
+        },
+        show_diff = {
+          normal = "gd",
+          insert = "<C-d>",
+        },
+        show_system_prompt = {
+          normal = "gp",
+        },
+        show_user_selection = {
+          normal = "gs",
+        },
+      },
+    },
+  },
+}
