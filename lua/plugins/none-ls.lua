b76eddcbe4a3bb161ece2710b78e2aa9b50a3512
Kiet Cao <kietcao@tuta.io>
2024-07-02 07:05:02 +0900

    [temp] custom prettier config file path

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "nvimtools/none-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      opts.sources = opts.sources or {}
+      table.insert(
+        opts.sources,
+        nls.builtins.formatting.prettier.with({
+          extra_args = { "--config", "lara/.prettierrc.js" },
+        })
+      )
+    end,
+  },
+}
