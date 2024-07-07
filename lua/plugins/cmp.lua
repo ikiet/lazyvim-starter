685f643077890f43f1be02e9639eebfa030be3f6
Kiet Cao <kietcao@tuta.io>
2024-07-07 16:37:08 +0900

    set presect mode to none

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -3,6 +3,13 @@ return {
   {
     "nvim-cmp",
     opts = {
+      mapping = cmp.mapping.preset.insert({
+        ["<CR>"] = LazyVim.cmp.confirm({ select = false }),
+      }),
+      completion = {
+        completeopt = "noselect",
+      },
+      preselect = cmp.PreselectMode.None,
       window = {
         completion = cmp.config.window.bordered(),
         documentation = cmp.config.window.bordered(),
