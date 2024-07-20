1485e127c88167559674e111d8eb2082b79b5b0a
Kiet Cao <kietcao@tuta.io>
2024-07-21 07:50:54 +0900

    add codesnap

---
+++ lua/plugins/codesnap.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "mistricky/codesnap.nvim",
+    build = "make",
+    opts = {
+      save_path = "~/Desktop",
+      has_breadcrumbs = true,
+      show_workspace = true,
+      watermark = "",
+      has_line_number = true,
+    },
+  },
+}
