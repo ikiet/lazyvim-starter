8bbc7071763581990cfb3f2bbbca7fa195b5c5e1
Kiet Cao <kietcao@tuta.io>
2024-07-28 15:05:08 +0900

    gitsigns: turn on number, line and word highlight

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      numhl      = true,
+      linehl     = true,
+      word_diff  = true,
+      current_line_blame = true,
+    },
+  },
+}
