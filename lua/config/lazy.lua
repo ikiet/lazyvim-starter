86da5611695f70dfc1980cc7b2033f4fab1e3ccf
Kiet Cao <kietcao@tuta.io>
2024-07-28 15:58:31 +0900

    add harpoon & refactoring

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -40,6 +40,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.util.project" },
     { import = "lazyvim.plugins.extras.util.rest" },
+    { import = "lazyvim.plugins.extras.editor.harpoon2" },
+    { import = "lazyvim.plugins.extras.editor.refactoring" },
     { import = "plugins" },
   },
   defaults = {
