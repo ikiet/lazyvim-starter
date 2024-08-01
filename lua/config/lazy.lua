0d62e5d348be1f6ef7c7b06d6099d45ab1324b0e
Kiet Cao <kietcao@tuta.io>
2024-08-01 23:50:12 +0900

    remove none-ls & unused format keymap

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -24,7 +24,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.inc-rename" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
-    { import = "lazyvim.plugins.extras.lsp.none-ls" },
+    -- { import = "lazyvim.plugins.extras.lsp.none-ls" },
     { import = "lazyvim.plugins.extras.ui.treesitter-context" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.php" },
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,16 +1,3 @@
 return {
-  {
-    "nvimtools/none-ls.nvim",
-    optional = true,
-    opts = function(_, opts)
-      local nls = require("null-ls")
-      opts.sources = opts.sources or {}
-      table.insert(
-        opts.sources,
-        nls.builtins.formatting.prettier.with({
-          extra_args = { "--config", "lara/.prettierrc.js" },
-        })
-      )
-    end,
-  },
+
 }
