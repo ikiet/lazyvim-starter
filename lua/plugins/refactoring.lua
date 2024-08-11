a611ad7583254427288b146d7e496262a9673aba
Kiet Cao <kietcao@tuta.io>
2024-08-11 17:37:01 +0900

    config refactoring debug print

---
+++ lua/plugins/refactoring.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "ThePrimeagen/refactoring.nvim",
+    opts = {
+        printf_statements = {
+        js = {
+          'console.log("-ctkiet-%s")',
+        },
+        ts = {
+          'console.log("-ctkiet-%s")',
+        },
+        javascript = {
+          'console.log("-ctkiet-%s")',
+        },
+        typescript = {
+          'console.log("-ctkiet-%s")',
+        },
+  },
+      print_var_statements = {
+        js = {
+          'console.log("-ctkiet-%s %%o", %s)',
+        },
+        ts = {
+          'console.log("-ctkiet-%s %%o", %s)',
+        },
+        javascript = {
+          'console.log("-ctkiet-%s %%o", %s)',
+        },
+        typescript = {
+          'console.log("-ctkiet-%s %%o", %s)',
+        },
+      },
+    },
+  },
+}
