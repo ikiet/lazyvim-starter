76624d76e544f0cfe46459b79a2a260c858f5d93
Kiet Cao <kietcao@tuta.io>
2024-08-13 13:02:37 +0900

    add jsx custom print statements

--- lua/plugins/refactoring.lua
+++ lua/plugins/refactoring.lua
@@ -1,34 +1,29 @@
+local js_print_var_statements = {
+  'console.log("-ctkiet-%s %%o", %s)',
+}
+local js_printf_statements = {
+  'console.log("-ctkiet-%s")',
+}
+
 return {
   {
     "ThePrimeagen/refactoring.nvim",
     opts = {
-        printf_statements = {
-        js = {
-          'console.log("-ctkiet-%s")',
-        },
-        ts = {
-          'console.log("-ctkiet-%s")',
-        },
-        javascript = {
-          'console.log("-ctkiet-%s")',
-        },
-        typescript = {
-          'console.log("-ctkiet-%s")',
-        },
-  },
+      printf_statements = {
+        js = js_printf_statements,
+        ts = js_printf_statements,
+        javascript = js_printf_statements,
+        typescript = js_printf_statements,
+        javascriptreact = js_printf_statements,
+        typescriptreact = js_printf_statements,
+      },
       print_var_statements = {
-        js = {
-          'console.log("-ctkiet-%s %%o", %s)',
-        },
-        ts = {
-          'console.log("-ctkiet-%s %%o", %s)',
-        },
-        javascript = {
-          'console.log("-ctkiet-%s %%o", %s)',
-        },
-        typescript = {
-          'console.log("-ctkiet-%s %%o", %s)',
-        },
+        js = js_print_var_statements,
+        ts = js_print_var_statements,
+        javascript = js_print_var_statements,
+        typescript = js_print_var_statements,
+        javascriptreact = js_print_var_statements,
+        typescriptreact = js_print_var_statements,
       },
     },
   },
