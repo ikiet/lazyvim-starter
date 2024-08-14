70b7cfb7dfda1e2f21d13f640b480cf8f8acc623
Kiet Cao <kietcao@tuta.io>
2024-08-14 22:10:07 +0900

    remove useless cmp config

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -3,13 +3,6 @@ return {
   {
     "nvim-cmp",
     opts = {
-      mapping = cmp.mapping.preset.insert({
-        ["<CR>"] = LazyVim.cmp.confirm({ select = false }),
-      }),
-      completion = {
-        completeopt = "noselect",
-      },
-      preselect = cmp.PreselectMode.None,
       window = {
         completion = cmp.config.window.bordered(),
         documentation = cmp.config.window.bordered(),
