0f015be1dddeca57e96b8622f8488a2ccd43d167
Kiet Cao <kietcao@tuta.io>
2024-08-15 10:14:52 +0900

    update cmp mapping
    - add fallback after close with C-e
    - add scroll docs mappings

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -3,6 +3,26 @@ return {
   {
     "nvim-cmp",
     opts = {
+      mapping = cmp.mapping.preset.insert({
+        ["<C-b>"] = function(fallback)
+          if cmp.visible() then
+            cmp.scroll_docs(-4)
+          else
+            fallback()
+          end
+        end,
+        ["<C-f>"] = function(fallback)
+          if cmp.visible() then
+            cmp.scroll_docs(4)
+          else
+            fallback()
+          end
+        end,
+        ["<C-e>"] = function(fallback)
+          cmp.close()
+          fallback()
+        end,
+      }),
       window = {
         completion = cmp.config.window.bordered(),
         documentation = cmp.config.window.bordered(),
