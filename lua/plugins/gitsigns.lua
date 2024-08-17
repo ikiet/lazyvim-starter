17cf4ed91fa5316dcaae84f7ca84277988418301
Kiet Cao <kietcao@tuta.io>
2024-08-17 16:08:05 +0900

    add gitsigns TODO

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,10 +1,11 @@
+-- TODO: use different colors for different types of changes
 return {
   {
     "lewis6991/gitsigns.nvim",
     opts = {
-      numhl      = true,
-      linehl     = true,
-      word_diff  = false,
+      numhl = true,
+      linehl = true,
+      word_diff = false,
       current_line_blame = true,
     },
   },
